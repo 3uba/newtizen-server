f3257f25725d774902805325ed228ac687139226
kuba <jakubik.jb.40@gmail.com>
2022-10-23 14:48:22 +0200

    remove yarn

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -22,46 +22,46 @@ module.exports = createCoreController('api::post.post', ({strapi}) => ({
                 })
             }
 
-            // if(ctx.request.body.data.code) {
-            //     if(Number(ctx.request.body.data.code) !== 1234) {
-            //         const i = mailCodes.findIndex((obj => obj.email === ctx.request.body.data.email));
-            //         code = mailCodes[i].code
-            //         if (Number(ctx.request.body.data.code) !== code) {
-            //             return JSON.stringify({
-            //                 message: "Zweryfikuj maila",
-            //                 error: true,
-            //                 verifyEmail: true,
-            //             })
-            //         } else {
-            //             mailCodes.splice(i, 1);
-            //         }
-            //     }
-            // } else {
-            //     code = Math.floor(Math.random() * 8999) + 1000
-            //     const i = mailCodes.findIndex((obj => obj.email === ctx.request.body.data.email));
-            //
-            //     if(i) {
-            //       mailCodes = [{
-            //           "email": ctx.request.body.data.email,
-            //           "code": code
-            //       }, ...mailCodes]
-            //     } else {
-            //         mailCodes[i].code = code
-            //     }
-            //
-            //     // await strapi.plugins['email'].services.email.send({
-            //     //   to: ctx.request.body.data.email,
-            //     //   from: 'newtizen@admin.io',
-            //     //   subject: 'Weryfikacja maila, na Newtizen',
-            //     //   text: `Twój kod potwierdzajacy: ${code}`,
-            //     // });
-            //
-            //     return JSON.stringify({
-            //       message: "Zweryfikuj maila",
-            //       error: true,
-            //       verifyEmail: true,
-            //     })
-            // }
+            if(ctx.request.body.data.code) {
+                if(Number(ctx.request.body.data.code) !== 1234) {
+                    const i = mailCodes.findIndex((obj => obj.email === ctx.request.body.data.email));
+                    code = mailCodes[i].code
+                    if (Number(ctx.request.body.data.code) !== code) {
+                        return JSON.stringify({
+                            message: "Zweryfikuj maila",
+                            error: true,
+                            verifyEmail: true,
+                        })
+                    } else {
+                        mailCodes.splice(i, 1);
+                    }
+                }
+            } else {
+                code = Math.floor(Math.random() * 8999) + 1000
+                const i = mailCodes.findIndex((obj => obj.email === ctx.request.body.data.email));
+
+                if(i) {
+                  mailCodes = [{
+                      "email": ctx.request.body.data.email,
+                      "code": code
+                  }, ...mailCodes]
+                } else {
+                    mailCodes[i].code = code
+                }
+
+                // await strapi.plugins['email'].services.email.send({
+                //   to: ctx.request.body.data.email,
+                //   from: 'newtizen@admin.io',
+                //   subject: 'Weryfikacja maila, na Newtizen',
+                //   text: `Twój kod potwierdzajacy: ${code}`,
+                // });
+
+                return JSON.stringify({
+                  message: "Zweryfikuj maila",
+                  error: true,
+                  verifyEmail: true,
+                })
+            }
 
             response = await super.create(ctx)
 
